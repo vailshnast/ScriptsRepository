5f3889645316b790a9fc2f9af2176c95df7682c9
vailshnast <vailll@yandex.ru>
2016-11-15 22:34:15 +0200

    Added Owner functions
    Added Owner functions

---
+++ PlanetNode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using GlobalVars;
+public class PlanetNode : MonoBehaviour {
+
+    public string planetName;
+    private Owner owner;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Owner GetOwner()
+    {
+        return owner;
+    }
+    public void SetOwner(Owner newOwner)
+    {
+        owner = newOwner;
+    }
+}
