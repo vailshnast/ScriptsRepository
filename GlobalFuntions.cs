42f89da9b965bb1570d82b980ebf0eb27a1afb76
vailshnast <vailll@yandex.ru>
2016-11-15 23:52:54 +0200

    Added this script
    Added this script

---
+++ GlobalFuntions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GlobalVars;
+
+
+namespace GlobalVars
+{
+    public class GlobalFuntions
+    {
+
+        GlobalVars.GameObjects.
+
+    }
+}
+
+    
