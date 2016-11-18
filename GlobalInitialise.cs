230a2f9b5955f1efeb7a44979ad4740c4dd2a3ee
vailshnast <vailll@yandex.ru>
2016-11-18 03:50:29 +0200

    Added several functions to Start
    Added several functions to Start

---
+++ GlobalInitialise.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using GlobalVars;
+public class GlobalInitialise : MonoBehaviour {
+
+	// Start is called after Awake
+	void Start()
+	{
+			GlobalFuntions.SetIDList(GameObjectLists.planetIDList, GameObjectLists.planetList.Count);
+			GlobalFuntions.SetPlanetNeighbours(GameObjectLists.planetIDList);
+	}
+
+	  // First is called Awake
+	  void Awake()
+	  {
+
+	  }
+}
