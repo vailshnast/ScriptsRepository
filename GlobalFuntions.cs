a03af47e4a739d6a475bcc6e603d5bff619cd3ff
vailshnast <vailll@yandex.ru>
2016-11-18 03:54:06 +0200

    Added several functions
    Added several functions

--- GlobalFuntions.cs
+++ GlobalFuntions.cs
@@ -1,16 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using GlobalVars;
+using System.Linq;
 
 
 namespace GlobalVars
 {
     public class GlobalFuntions
-    {
+    {      
+        public static PlanetNode GetNode(int ID)
+        {
+            for (int i = 0; i < GameObjectLists.planetList.Count; i++)
+            {
+                if (GameObjectLists.planetList[i].GetComponent<PlanetNode>().GetID() == ID)
+                    return GameObjectLists.planetList[i];
+            }
+            return null;		 
+		 }
 
-        GlobalVars.GameObjects.
+        public static List<int> SetIDList(List<int> IDList, int Count)
+        {
+            IDList.Clear();
+            for (int i = 0; i < Count; i++)
+            {
+                IDList.Add(i);
+            }
+            return IDList;
 
+        }
+        public static List<int> SetPlanetNeighbours(List<int> IDList)
+        {
+            for (int i = 0; i < IDList.Count; i++)
+            {
+                GameObjectLists.planetNeighbours.Add(GetNode(i).GetConnectedPlanets());
+            }
+            return null;
+        }
     }
+
 }
 
-    
+
