3a86b885967c4805ed27936ac9b676b5f70ba19f
vailshnast <vailll@yandex.ru>
2016-11-18 03:54:33 +0200

    Added several funtions
    Added several funtions

--- PlanetNode.cs
+++ PlanetNode.cs
@@ -2,41 +2,59 @@ using UnityEngine;
 using System.Collections.Generic;
 using GlobalVars;
 
-
-public class PlanetNode : MonoBehaviour {
-
-    public string planetName;
-    public int planetID;
-
-    private Owner owner;
-
-	[System.Serializable]
-    private List<PlanetNode> connectedPlanet = new List<PlanetNode>();
-
-	// Use this for initialization
-	void Start () {
-        Initialise();
-        Debug.Log(GameObjects.planetList);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    public Owner GetOwner()
+    public class PlanetNode : MonoBehaviour
     {
-        return owner;
-    }
-    public void SetOwner(Owner newOwner)
-    {
-        owner = newOwner;
-    }
 
-    protected void Initialise()
-    {
-        GameObjects.planetList.Add(this);
-    }
-
+        public string planetName;
     
-}
+        private Owner owner;
+
+        [SerializeField]
+        private List<PlanetNode> connectedPlanets = new List<PlanetNode>();
+
+        [SerializeField]
+        private int planetID;
+
+    // Use this for initialization
+		void Awake()
+        {
+            Initialise();
+		}
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public Owner GetOwner()
+        {
+            return owner;
+        }
+        public void SetOwner(Owner newOwner)
+        {
+            owner = newOwner;
+        }
+        public int[] GetConnectedPlanets()
+        {
+			int[] connectedPlanetsArray = new int[connectedPlanets.Count];
+
+			for (int i = 0; i < connectedPlanetsArray.Length; i++)
+			{
+				  connectedPlanetsArray[i] = connectedPlanets[i].planetID;				  
+			}
+			return connectedPlanetsArray;
+	    }
+   	
+                 
+        public int GetID()
+        {
+            return planetID;
+        }
+        protected void Initialise()
+        {
+            GameObjectLists.planetList.Add(this);
+        }
+
+
+    }
