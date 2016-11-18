d65e2d87a86ab441abbe15cc228050d8a57bddb0
vailshnast <vailll@yandex.ru>
2016-11-18 03:48:25 +0200

    Added several lists
    Added several lists

---
+++ GameObjectLists.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GlobalVars;
+
+namespace GlobalVars
+{
+    public class GameObjectLists
+    {
+        public static List<int> planetIDList = new List<int>();
+
+        public static List<PlanetNode> planetList = new List<PlanetNode>();
+
+        public static List<int[]> planetNeighbours = new List<int[]>();
+    }
+    
+}
